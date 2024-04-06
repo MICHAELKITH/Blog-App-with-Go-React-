2c235d07b8487a95085e15844f56cf4fab2d4e2c
MICHAELKITH <michaelkithinji27@gmail.com>
2024-04-06 22:48:28 +0300

    set route

--- server/server.go
+++ server/server.go
@@ -6,7 +6,7 @@ func main() {
 
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.JSON(fiber.Map{"message": "Pro mike"})
+		return c.JSON(fiber.Map{"message": "Pro mike is a Go "})
 	})
 	app.Listen(":8000")
 }
