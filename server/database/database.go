3efbeb52ba7a31a066b767d57cb8f143e25c5af4
MICHAELKITH <michaelkithinji27@gmail.com>
2024-04-07 08:56:59 +0300

    Implement postgreSQL server connection

---
+++ server/database/database.go
@@ -0,0 +1,8 @@
+package database
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+  )
+  
+  dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
