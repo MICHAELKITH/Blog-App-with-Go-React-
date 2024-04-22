ff99a97105d5e549b6814afb315b4bd2ec31d0b9
MICHAELKITH <michaelkithinji27@gmail.com>
2024-04-22 23:59:28 +0300

    Initialize database connection

--- server/database/database.go
+++ server/database/database.go
@@ -1,13 +1,24 @@
 package database
+
 import (
-  "database/sql"
-  "fmt"
-  _ "github.com/lib/pq"
-  )
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
   
   func database (){
     dsn := "host=localhost user=postgres password=secretkey dbname=postblog port=5432 sslmode=disable"
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    defer db.close()
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    if err = db.Ping(); err != nil{
+      log.Fatal(err)
+    }
     
   }
  
