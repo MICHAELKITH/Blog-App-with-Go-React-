e81821ebc1672388419f748495675ec02e7744b4
MICHAELKITH <michaelkithinji27@gmail.com>
2024-04-23 00:55:41 +0300

    Create struct for DB

--- server/database/database.go
+++ server/database/database.go
@@ -2,24 +2,34 @@ package database
 
 import (
 	"database/sql"
+	"gorm.io/driver/postgres"
 	"log"
 
 	_ "github.com/lib/pq"
 	"gorm.io/gorm"
 )
- 
-var DBconn *gorm.DB
-  func ConnectDB (){
-    dsn := "host=localhost user=postgres password=secretkey dbname=postblog port=5432 sslmode=disable"
-    db, err := gorm.Open(sql.Open(dsn), &gorm.Config{})
-    defer db.close()
-    if err != nil {
-      log.Fatal(err)
-    }
 
-    if err = db.Ping(); err != nil{
-      log.Fatal(err)
-    }
-    
-  }
- 
+type Config struct {
+	Host     string
+	Port     string
+	Password string
+	User     string
+	DBname   string
+	SSLMode  string
+}
+
+var DBconn *gorm.DB
+
+func ConnectDB() {
+	dsn := "host=localhost user=postgres password=secretkey dbname=postblog port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	defer db.close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+}
